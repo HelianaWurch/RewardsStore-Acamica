1e7ff9337a760cb6b2e6d5708b643b29f085e19b
Heli <wurch.heliana@gmail.com>
2021-07-25 03:19:40 -0300

    [HW02]Connect to api. Create AllProductsContext. Draw products info.

--- src/scenes/SearchProducts/SearchProducts.js
+++ src/scenes/SearchProducts/SearchProducts.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import Filters from "./components/Filters";
 import Product from "./components/Product";
+import { AllProductsContext } from "../../contexts/AllProductsContext";
 
 function SearchProducts() {
+	const { isLoading, products } = useContext(AllProductsContext);
+
+	console.log(products);
 	return (
 		<section className="container">
 			<div>
 				<Filters />
 			</div>
 			<div className="justify-content-around my-3 row">
-				<Product />
-				<Product />
-				<Product />
-				<Product />
-				<Product />
-				<Product />
+				{products.map((product) => (
+					<Product key={product._id} product={product} />
+				))}
 			</div>
 		</section>
 	);
